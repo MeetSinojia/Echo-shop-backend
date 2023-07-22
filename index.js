8e43c00bfb42d37cd01cfb7c353b8308fc71a037
Meet <sinojiameet321@gmail.com>
2023-07-22 18:37:27 +0530

    added location feature

--- index.js
+++ index.js
@@ -6,13 +6,81 @@ const stripe = require("stripe")(
 );
 
 const app = express();
+const fs = require("fs");
 
 app.use(cors());
 app.use(express.json());
+const citiesData = JSON.parse(fs.readFileSync("cities_data.json", "utf-8"));
 
 app.get("/", (req, res) => {
     res.status(200).send("Hello, world!");
 });
+
+// Function to build a graph from citiesData
+function buildGraph() {
+    const graph = {};
+    for (const data of citiesData) {
+        const origin = data["Origin City"];
+        const destination = data["Destination City"];
+        const prediction = data["Average Prediction"];
+
+        if (!graph[origin]) {
+            graph[origin] = [];
+        }
+
+        graph[origin].push({ city: destination, prediction });
+    }
+    return graph;
+}
+// Helper function to find indirect paths recursively
+function findIndirectPath(graph, currentCity, targetCity) {
+    if (!graph[currentCity]) {
+        return null;
+    }
+
+    for (const { city, prediction } of graph[currentCity]) {
+        if (city === targetCity) {
+            return prediction;
+        } else {
+            const days = findIndirectPath(graph, city, targetCity);
+            if (days !== null) {
+                return days + prediction;
+            }
+        }
+    }
+
+    return null;
+}
+
+app.get("/estimatedDeliveryTime", (req, res) => {
+    const sellerLocation = req.query.sellerLocation;
+    const buyerLocation = req.query.buyerLocation;
+    console.log(buyerLocation);
+    console.log(sellerLocation);
+    const graph = buildGraph();
+    const directConnection = citiesData.find((data) => {
+        const origin = data["Origin City"];
+        const destinationCity = data["Destination City"];
+        return destinationCity === buyerLocation && sellerLocation === origin;
+    });
+
+    if (directConnection) {
+        const estimatedDeliveryTime = `${directConnection["Average Prediction"]} days`;
+        res.status(200).json({ estimatedDeliveryTime });
+    } else {
+        const indirectDays = findIndirectPath(graph, sellerLocation, buyerLocation);
+        if (indirectDays !== null) {
+            const estimatedDeliveryTime = `${indirectDays} days`;
+            res.status(200).json({ estimatedDeliveryTime });
+        } else {
+            console.log("Delivery route not found");
+            res.status(404).json({ error: "Delivery route not found" });
+        }
+    }
+});
+
+
+
 
 app.post("/payments/create", async (req, res) => {
     const total = req.query.total;
@@ -31,6 +99,8 @@ app.post("/payments/create", async (req, res) => {
         clientSecret: paymentIntent.client_secret
     });
 });
+
+
 
 app.listen(5001, () => {
     console.log("Server is running on http://localhost:5001");
